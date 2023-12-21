0ba6c2ae97d5ebf92fb58768a084bda21b601cc9
Forrest Klausmeier <fwklausmeier@gmail.com>
2023-12-21 16:13:40 -0700

    Adding Screens and Navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
 // import type {PropsWithChildren} from 'react';
 import {
@@ -11,6 +12,7 @@ import {
   useColorScheme,
   View,
   Image,
+  Button,
 } from 'react-native';
 
 // import {
@@ -26,25 +28,142 @@ import {
 // }>;
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function LoadingScreen({ navigation }) {
   return (
-    <SafeAreaView
-      onPress={() => navigation.navigate('HomeScreen')}
-      style={styles.loading}
-    >
-      <Image style={styles.image} source={require('./images/order_weasel_small.jpg')} />
-      <Text  style={styles.loadingText}>SEARCHING FOR RESTAURANTS...</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Image source={require('./images/order_weasel_small.jpg')} />
+        <Text  style={[styles.loadingText, {color: 'blue'}]}>SEARCHING FOR RESTAURANTS...</Text>
+      </View>
+
+      <View>
+        <Button title="Temporary: go to home page" onPress={()=> navigation.navigate('HomeScreen')}/>
+      </View>
     </SafeAreaView>
   );
 }
 
-function HomeScreen({ navigation }) {
-
-  debugger;
+function Example() {
   return (
-    <SafeAreaView style={styles.main}>
-      <Text style={styles.mainText}>Main screen</Text>
+    <SafeAreaView>
+      <Text>Word</Text>
+    </SafeAreaView>
+  );
+}
+
+// function HowItWorksN() {
+//   let text = [
+//     'Select from any one of our participating merchants',
+//     'Add items to your cart and place your order',
+//     "Use the time estimate to determine when you need to arrive for pickup. We'll still send you a notification to let you know when the order's ready anyway.",
+//     'When you arrive at the restaurant, your order should be waiting for you. Just pay at the counter and be on your way!'
+//   ];
+//
+//   return(
+//     <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+//       <Text style={{flex: 1, color: 'black'}}>How it Works(1)</Text>
+//       <Text>{text[0]}</Text>
+//     </SafeAreaView>
+//   );
+// }
+
+function HomeTab({navigation}) {
+  return(
+  <SafeAreaView>
+    <View style={styles.mainTextContainer}>
+      <Text style={{color: 'black'}}>
+        Location
+      </Text>
+    </View>
+    <View></View>
+    <View style={styles.mainTextContainer}>
+      <Text style={{color: 'black'}}>
+        Restaurants Near You (Carryout Only)
+      </Text>
+    </View>
+    <View style={styles.mainTextContainer}>
+     <Text style={{color: 'black'}}>
+        1 result
+     </Text>
+    </View>
+    <View style={styles.mainTextContainer}>
+      <Button title="Temporary: Go To Red Pickle" onPress={() => navigation.navigate("Example")}/>
+    </View>
+  </SafeAreaView>
+  );
+}
+
+function ServicesTab() {
+  let text = [
+    'Select from any one of our participating merchants',
+    'Add items to your cart and place your order',
+    "Use the time estimate to determine when you need to arrive for pickup. We'll still send you a notification to let you know when the order's ready anyway.",
+    'When you arrive at the restaurant, your order should be waiting for you. Just pay at the counter and be on your way!'
+  ];
+
+  return(
+     <SafeAreaView>
+       <Text style={{color: 'black', textAlign: 'center', marginBottom: 16, fontSize: 32}}>Our Services</Text>
+{/*        <Stack.Navigator> */}
+{/*          <Stack.Screen name="HowItWorks(1)" component={HowItWorksN}/> */}
+{/*          <Stack.Screen name="HowItWorks(2)" component={HowItWorksN}/> */}
+{/*          <Stack.Screen name="HowItWorks(3)" component={HowItWorksN}/> */}
+{/*          <Stack.Screen name="HowItWorks(4)" component={HowItWorksN}/> */}
+{/*        </Stack.Navigator> */}
+        <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>-{text[0]}</Text>
+        <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>-{text[1]}</Text>
+        <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>-{text[2]}</Text>
+        <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>-{text[3]}</Text>
+     </SafeAreaView>
+  );
+}
+
+function TermsTab() {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text style={{color: 'black', textAlign: 'center', marginBottom: 16, fontSize: 32}}>Terms of Service</Text>
+      </View>
+      <View >
+        <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>
+          This is a student project...
+        </Text>
+       <Text style={{color: 'black', marginBottom: 10, fontSize: 16}}>
+          We are not liable...
+       </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: 'yellow',
+      },
+      tabBarIconStyle: {
+        display: 'none',
+      },
+      tabBarLabelStyle: {
+        fontSize: 16,
+        marginBottom: 10,
+      }
+    }}>
+      <Tab.Screen name="Home" component={ HomeTab }/>
+      <Tab.Screen name="Our Services" component={ServicesTab}/>
+      <Tab.Screen name="Terms" component={TermsTab}/>
+    </Tab.Navigator>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: "white"}]}>
+        <MyTabs />
     </ SafeAreaView>
   );
 }
@@ -54,12 +173,27 @@ function App(): React.JSX.Element {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Loading Screen"
+          name="Loading"
           component={LoadingScreen}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-          name="Home Screen"
+          name="HomeScreen"
           component={HomeScreen}
+          options={{
+            title: "OrderWeasel",
+            headerTintColor: "blue",
+            headerStyle: {
+              backgroundColor: "#FBF501",
+            },
+            headerTitleAlign: "center",
+          }}
+        />
+        <Stack.Screen
+          name="Example"
+          component={Example}
         />
       </Stack.Navigator>
     </ NavigationContainer>
@@ -67,31 +201,34 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  loading: {
+  container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
     backgroundColor: '#FBF501',
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   main: {
     flex: 1,
     backgroundColor: "white",
   },
-  mainText: {
+  mainTextContainer: {
     textAlign: "center",
-    color: "black",
-  },
-  image: {
-    top: -30,
-    width: 100,
+    height: 60,
+    backgroundColor: "white",
+    borderColor: 'black',
+    borderBottomWidth: 1,
   },
   loadingText: {
     width: 275,
-    top: 20,
-    left: 15,
     color: 'black',
+    marginTop: 20,
+    textAlign: "center"
   }
 });
-
 
 export default App;
