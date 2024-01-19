3d347bcd4eea345dbcb562e4943a376ed326bc90
Forrest Klausmeier <fwklausmeier@gmail.com>
2024-01-19 11:08:26 -0700

    MenuTab Refactored

--- src/tab-navigator/MenuTab.tsx
+++ src/tab-navigator/MenuTab.tsx
@@ -7,21 +7,141 @@ import {
   StyleSheet,
   Pressable,
   Image,
-  RefreshControl,
+  Modal,
+  RefreshControl
 } from 'react-native'
-import { CartScreen } from '../screens/CartScreen';
-import { MenuItem } from '../components/list/MenuItem';
-import { MenuScreenHeader } from '../components/screen/MenuScreenHeader';
-import { MenuScreenFooter } from '../components/screen/MenuScreenFooter';
-import { MenuListHeader } from '../components/list/MenuListHeader';
+import { CartModal } from '../components/ui/CartModal';
+import { GetStars } from '../components/api/GetStars';
+import { GetDirections } from '../components/api/GetDirections';
 
-import { textStyles } from '../res/styles/text'
-import { containerStyles } from '../res/styles/container'
+import { textStyles } from '../res/styles/text';
+import { containerStyles } from '../res/styles/container';
+import { buttonStyles } from '../res/styles/button';
 import menuData from '../data/menuData.js'
 
 // fetching menu data for a restaurant example
 function getMenuData(id) {
   return menuData[id];
+}
+
+export function MenuScreenHeader({params}) {
+  let logo = '../../res/images/order_weasel_small.jpg'
+  let id = params.id;
+  let title = params.title;
+
+  return (
+    <View style={[ containerStyles.headerContainer, {flex: 0.12} ]}>
+      <View style={containerStyles.headerLogoContainer}>
+        <Image style={containerStyles.logoSize} source={require(logo)}/>
+      </View>
+     <Text style={[textStyles.text, textStyles.smallHeadings, {flex: 3}]}>{title}</Text>
+    </View>
+  );
+}
+
+export function MenuListHeader({id, title, category, distance, rating, phone, hours, address}) {
+  // example
+  hours = '8:00-8:00';
+  phone = '(456)567-7895'
+
+  let clock = '../res/images/clock.png';
+  let phonePic = '../res/images/phone.png';
+
+  return (
+    <View style={{flex: 1}}>
+      <View style={[styles.topSection, {flex: 1}]}>
+        <View style={{flex: 1, flexDirection: 'column', margin: 10}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <View style={{flex: 3}}>{/*Spacer*/}</View>
+              <View style={{flex: 2}}>
+                <Text style={[textStyles.headingText, {fontSize: 18, textAlign: 'center'}]}>{rating}</Text>
+              </View>
+              <View style={{flex: 1}}>{/*Spacer*/}</View>
+            </View>
+            {/*Need stars Image based on rating*/}
+            <View style={{flex: 5, flexDirection: 'row', justifyContent: 'flex-start'}}>
+              <GetStars rating={rating}/>
+              <Text style={[textStyles.headingText, {fontSize: 18}]}>{category} | {distance}(mi)</Text>
+            </View>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <View style={{flex: 3}}>{/*Spacer*/}</View>
+              <View style={{flex: 2}}>
+                <Image style={{width: 20, height: 20}} source={require(clock)}/>
+              </View>
+              <View style={{flex: 1}}>{/*Spacer*/}</View>
+            </View>
+            <Text style={[textStyles.headingText, {flex: 5, fontSize: 18}]}>{hours}</Text>
+          </View>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <View style={{flex: 3}}>{/*Spacer*/}</View>
+              <View style={{flex: 2}}>
+                <Image style={{width: 20, height: 20}} source={require(phonePic)}/>
+              </View>
+              <View style={{flex: 1}}>{/*Spacer*/}</View>
+            </View>
+            <Text style={[textStyles.headingText, {flex: 5, fontSize: 18}]}>{phone}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={[styles.topSection, {padding: 10, flex: 1}]}>
+        <Text style={textStyles.text}>This is a pickup order. You'll need to go to {title} to pick up this order at:</Text>
+        <GetDirections address={address} />
+      </View>
+    </View>
+  );
+}
+
+export function MenuItem({item, restaurantId}) {
+  let name = item.name;
+  let cost = item.cost;
+  let desc = item.description;
+//   let url = item.picture;
+
+  // how are we going to get the picture for the item since we can't call require dynamically?
+  // for demonstration purposes
+  let url = '../res/images/order_weasel_small.jpg';
+
+  // modal state
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return(
+    <View>
+      <CartModal modalVisible={modalVisible} setModalVisible={setModalVisible} item={item} restaurantId={restaurantId}/>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <View style={[containerStyles.itemContainer, {flexDirection: 'row', padding: 8}]}>
+         <View style={{flex: 2, marginLeft: 16}}>
+            <Text style={[textStyles.text, {flex: 1}]}>{name}</Text>
+            <Text style={[textStyles.text, {flex: 1, fontSize: 12}]}>{desc}</Text>
+            <Text style={[textStyles.text, {flex: 1, fontSize: 12}]}>{cost}</Text>
+         </View>
+         <View style={{flex: 1}}>
+           <Image style={{marginRight: 32, width: 75, height: 75}} source={require(url)} />
+         </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export function MenuScreenFooter({params, navigation}) {
+  return (
+    <Pressable style={[buttonStyles.bottomNav, {flex: 0.11}]} onPress={()=> navigation.navigate('Cart', params)}>
+      <View style={{flex: 0.5}}></View>
+      <View style={[textStyles.buttonText, containerStyles.justifyText]}>
+        <View style={{flex: 1}}>
+          <Text style={[textStyles.headingText, {textAlign: 'center'}]}>View Cart</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Text style={[textStyles.headingText,  {textAlign: 'center'}]}>$0.00</Text>
+        </View>
+      </View>
+      <View style={{flex: 0.5}}></View>
+    </Pressable>
+  );
 }
 
 export function MenuTab({route, navigation}) {
@@ -115,3 +235,11 @@ export function MenuTab({route, navigation}) {
     </ SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  topSection: {
+     backgroundColor: 'white',
+     borderColor: 'black',
+     borderBottomWidth: 1,
+   }
+});
